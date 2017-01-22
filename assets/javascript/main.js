dd7533373e56f4c85d7db1457a3f5b7b82f28e3c
thechutrain <chu.chuyalan@gmail.com>
2017-01-22 13:29:00 -0500

    basic form displays for new users and registered users

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,19 @@
-// alert("testing if refs properly");
+$(document).ready(function(){
+  // hide the login form
+  $("form#sign-in-form").hide();
+
+  // event listener for already registered login
+  $("#sign-up-form").find("p.form-text-link").on("click", function(){
+      console.log("EVENT fired");
+      $("form#sign-in-form").show();
+      $("form#sign-up-form").hide();
+  })
+
+  // event listener for new user
+  $("#sign-in-form").find("p.form-text-link").on("click", function(){
+      console.log("sign in form event!");
+      $("form#sign-in-form").hide();
+      $("form#sign-up-form").show();
+  })
+
+});
